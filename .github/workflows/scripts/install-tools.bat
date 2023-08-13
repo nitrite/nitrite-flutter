c76db9598eac9873dbc70322349ebd90f6a0f54f
Anindya Chatterjee <anidotnet@gmail.com>
2023-08-13 21:59:49 +0530

    delete build scripts

--- .github/workflows/scripts/install-tools.bat
+++
@@ -1,5 +0,0 @@
-CMD /C dart pub global activate melos
-CMD /C dart pub global activate dartdoc
-REM Workaround an issue when running global executables on Windows for the first time.
-CMD /C melos > NUL
-melos bootstrap
--- .github/workflows/scripts/install-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dart pub global activate melos
-dart pub global activate dartdoc
-melos bootstrap
